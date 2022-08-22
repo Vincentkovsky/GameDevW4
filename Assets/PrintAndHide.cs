996c9e3c6ab35d9580dd7cfbbf5e46634c4852a2
Vincentkovsky <57795931+Vincentkovsky@users.noreply.github.com>
2022-08-22 21:51:16 +1000

    last'

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -7,11 +7,15 @@ public class PrintAndHide : MonoBehaviour
 
     private int i;
     public Renderer rend;
+    private int random;
+    public GameObject redObj;
+
     
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        random = Random.Range(200,251);
     }
 
     // Update is called once per frame
@@ -19,5 +23,12 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(rend.name+":"+i);
+        if(rend.tag == "Red" && i == 100){
+            redObj = GameObject.Find("RedPrefab(Clone)");
+            redObj.SetActive(false);
+        }
+        if(rend.tag == "Blue" && i == random){
+            rend.enabled = false;
+        }
     }
 }
